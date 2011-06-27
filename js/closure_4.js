e0c643cb7ba80e2d190db75806bd23af17e131fa
duroflex <k.varmark@gmail.com>
2011-06-27 06:47:36 -0700

    Edited js/closure_4.js via GitHub

--- js/closure_4.js
+++ js/closure_4.js
@@ -1,1 +1,34 @@
-/**	Dynamically create functions with common behaviour		Minified and private properties*/var closure = function () {	var string = 'Hi my name is ';		var makeName = function greeting (name) {		name = (name || 'Conan');				return function (lastName) {			console.log(string + name + ' ' + lastName);		};	}	return {		makeName : makeName	};}();/**	examples*/console.log("\n\n");console.log('Closure 4');var logConanFunc = closure.makeName();logConanFunc('The Barbarian');closure.makeName('Dewey')('The Librarian');/**/
+/**
+	Dynamically create functions with common behaviour
+	
+	Minified and private properties
+*/
+
+var closure = function () {
+	var string = 'Hi my name is ';
+	
+	var makeName = function greeting (name) {
+		name = (name || 'Conan');
+		
+		return function (lastName) {
+			console.log(string + name + ' ' + lastName);
+		};
+	}
+
+	return {
+		makeName : makeName
+	};
+}();
+
+/**
+	examples
+*/
+console.log("\n\n");
+console.log('Closure 4');
+
+var logConanFunc = closure.makeName();
+logConanFunc('The Barbarian');
+
+closure.makeName('Dewey')('The Librarian');
+
+/**/
