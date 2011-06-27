48d5abb2b069f4b65abdb0a87bacd6a731a2f0ed
duroflex <k.varmark@gmail.com>
2011-06-27 06:42:26 -0700

    Edited js/closure_3.js via GitHub

--- js/closure_3.js
+++ js/closure_3.js
@@ -1,1 +1,34 @@
-/**	Dynamically create functions with common behaviour		Doesn't work - why?*/var closure = function (name) {	var string = 'Hi my name is ';		var greeting = function (lastName) {		name = (name || 'Conan');		console.log(string + name + ' ' + lastName);	};	return {			makeName : function (name) {			return greeting;		}	};};/**	examples*/console.log("\n\n");console.log('Closure 3');var logConanFunc = closure().makeName('Skippy');logConanFunc('The Barbarian');closure().makeName('Dewey')('The Librarian');/**/
+/**
+	Dynamically create functions with common behaviour
+	
+	Doesn't work - why?
+*/
+
+var closure = function (name) {
+	var string = 'Hi my name is ';
+	
+	var greeting = function (lastName) {
+		name = (name || 'Conan');
+		console.log(string + name + ' ' + lastName);
+	};
+
+	return {
+	
+		makeName : function (name) {
+			return greeting;
+		}
+	};
+};
+
+/**
+	examples
+*/
+console.log("\n\n");
+console.log('Closure 3');
+
+var logConanFunc = closure().makeName('Skippy');
+logConanFunc('The Barbarian');
+
+closure().makeName('Dewey')('The Librarian');
+
+/**/
